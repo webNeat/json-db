32ac26489423ea2f49b97217b2bbe920ea4ae654
abenhammou <webneat@gmail.com>
2016-06-03 16:59:29 +0200

    added a types definition

---
+++ src/lib/types.js
@@ -0,0 +1,6 @@
+/**
+ * The database type.
+ * @typedef {Object} DB
+ * @property {String} folder - The path of the folder where the data files are stored.
+ * @property {Object<String, Schema>} schemas - The schemas on the database.
+ */
