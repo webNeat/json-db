cd1be2d393c07df06f788577f7f4469508f85983
abenhammou <webneat@gmail.com>
2016-06-03 17:24:20 +0200

    testing type definitions

---
+++ src/lib/types.js
@@ -0,0 +1,6 @@
+/**
+ * The database type.
+ * @typedef {Object} DB
+ * @prop {String} folder - The path of the folder where the data files are stored.
+ * @member {Object<String, Schema>} schemas - The schemas on the database.
+ */
