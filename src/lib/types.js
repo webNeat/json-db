4a9fe4f7a79beb299dbbdb52a23b686a0667865a
Amine Ben hammou <webneat@gmail.com>
2016-06-12 01:03:55 +0200

    eddited types

--- src/lib/types.js
+++ src/lib/types.js
@@ -2,5 +2,5 @@
  * The database type.
  * @typedef {Object} DB
  * @property {String} folder - **String** : The path of the folder where the data files are stored.
- * @property {Object<String, Schema>} schemas - **Object<String, Schema>** : The schemas on the database.
+ * @property {Object<String, Schema>} schemas - `Object<String, Schema>` : The schemas on the database.
  */
